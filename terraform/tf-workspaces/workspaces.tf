d86d2a164f80eee75eccd44f3f7defc2098ef962
chhokara <chhokara@gmail.com>
2025-09-09 19:02:54 -0700

    setup VCS integration for TFE

--- terraform/tf-workspaces/workspaces.tf
+++ terraform/tf-workspaces/workspaces.tf
@@ -10,9 +10,19 @@ terraform {
 provider "tfe" {
     token = var.tfc_api_token
 }
+data "tfe_github_installation" "this" {
+    name = "chhokara"
+}
 
 resource "tfe_workspace" "gopro_data_project" {
     name         = "gopro-data-project"
     organization = "gopro-data-project"
     description  = "A workspace for the GoPro Data Project"
+
+    vcs_repo {
+        identifier     = "chhokara/gopro-data-project"
+        branch         = "main"
+        github_app_installation_id = data.tfe_github_installation.this.id
+        ingress_submodules = false
+    }
 }
