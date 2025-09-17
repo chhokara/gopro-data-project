9b40c32f798aacdca4e0b111315314daa9b18aaf
chhokara <chhokara@gmail.com>
2025-09-16 19:03:49 -0700

    update working directory

--- terraform/workspaces.tf
+++ terraform/workspaces.tf
@@ -20,6 +20,8 @@ resource "tfe_workspace" "gopro-data-workspace" {
     name = "gopro-data-workspace"
     organization = tfe_organization.gopro-data-org.name
     queue_all_runs = false
+    working_directory = "./terraform"
+    auto_apply = true
     vcs_repo {
         branch = "main"
         identifier = "chhokara/gopro-data-project"
