0579a607818d5290603002ddc2a38dde7efe7bb5
chhokara <chhokara@gmail.com>
2025-09-18 19:39:24 -0700

    change workspace name

--- terraform/workspaces.tf
+++ terraform/workspaces.tf
@@ -25,7 +25,7 @@ resource "tfe_organization" "gopro-data-org" {
 }
 
 resource "tfe_workspace" "gopro-data-workspace" {
-    name = "gopro-data-workspace"
+    name = "gopro-data-ws"
     organization = tfe_organization.gopro-data-org.name
     queue_all_runs = false
     working_directory = "./terraform"
