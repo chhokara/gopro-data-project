44a3752a21386dcb9fa64445b1b7242249dc79f1
chhokara <chhokara@gmail.com>
2025-09-29 08:51:16 -0700

    build out batch and outputs

--- terraform/modules/batch/batch.tf
+++ terraform/modules/batch/batch.tf
@@ -27,3 +27,42 @@ resource "aws_batch_compute_environment" "this" {
         security_group_ids = [aws_security_group.batch.id]
     }
 }
+
+resource "aws_batch_job_queue" "this" {
+    name = "${var.job_name}-job-queue"
+    state = "ENABLED"
+    priority = 1
+    compute_environments = [aws_batch_compute_environment.this.arn]
+}
+
+locals {
+    container_props = jsonencode({
+        image = var.job_image
+        command = ["python", "/app/extract.py",
+                    "--input", "s3://${var.raw_bucket}",
+                    "--output", "s3://${var.curated_bucket}"]
+        environment = [
+            { name = "RAW_BUCKET", value = var.raw_bucket },
+            { name = "CURATED_BUCKET", value = var.curated_bucket }
+        ]
+        executionRoleArn = aws_iam_role.batch_execution_role.arn
+        jobRoleArn = aws_iam_role.batch_job_role.arn
+        resourceRequirements = [
+            { type = "VCPU", value = "1" },
+            { type = "MEMORY", value = "2048" }
+        ]
+        networkConfiguration = {
+            assignPublicIp = "DISABLED"
+        }
+    })
+}
+
+data "aws_region" "current" {}
+
+resource "aws_batch_job_definition" "this" {
+    name = "${var.job_name}-job-definition"
+    type = "container"
+    platform_capabilities = ["FARGATE"]
+    propagate_tags = true
+    container_properties = local.container_props
+}
--- terraform/modules/batch/output.tf
+++ terraform/modules/batch/output.tf
@@ -7,3 +7,13 @@ output "batch_execution_role_arn" {
     value = aws_iam_role.batch_execution_role.arn
     description = "ARN of the IAM role for Batch job execution"
 }
+
+output "batch_job_queue_arn" {
+    value = aws_batch_job_queue.this.arn
+    description = "ARN of the Batch job queue"
+}
+
+output "batch_job_definition_arn" {
+    value = aws_batch_job_definition.this.arn
+    description = "ARN of the Batch job definition"
+}
