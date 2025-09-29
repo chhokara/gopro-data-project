45d5ba4d4d41da9e5c5ed5f773e51a290edbc6aa
chhokara <chhokara@gmail.com>
2025-09-28 18:31:30 -0700

    move and comment out vpc module block

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,21 @@ module "raw_bucket" {
 module "eventbridge" {
   source = "./modules/eventbridge"
 }
+
+# module "vpc" {
+#     source = "../vpc"
+
+#     name = "gopro-vpc"
+#     cidr = "10.20.0.0/16"
+#     az_count = 2
+
+#     public_subnet_cidrs = ["10.20.0.0/24", "10.20.1.0/24"]
+#     private_subnet_cidrs = ["10.20.100.0/24", "10.20.101.0/24"]
+
+#     enable_nat_gateway = true
+
+#     tags = {
+#         project = "gopro"
+#         managed = "terraform"
+#     }
+# }
--- terraform/modules/batch/batch.tf
+++ terraform/modules/batch/batch.tf
@@ -1,21 +1,3 @@
-module "vpc" {
-    source = "../vpc"
-
-    name = "gopro-vpc"
-    cidr = "10.20.0.0/16"
-    az_count = 2
-
-    public_subnet_cidrs = ["10.20.0.0/24", "10.20.1.0/24"]
-    private_subnet_cidrs = ["10.20.100.0/24", "10.20.101.0/24"]
-
-    enable_nat_gateway = true
-
-    tags = {
-        project = "gopro"
-        managed = "terraform"
-    }
-}
-
 resource "aws_security_group" "batch" {
     name = "gopro-batch-sg"
     description = "Security group for AWS Batch"
