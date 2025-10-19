507ff069aafc7147ba2666188330102aa0d8187e
chhokara <chhokara@gmail.com>
2025-10-18 18:31:47 -0700

    fix provider block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,4 +23,9 @@ provider "tfe" {
   token = var.tfc_api_token
 }
 
-provider "google" {}
+provider "google" {
+  project = env("GCP_CLOUD_PROJECT")
+  region  = env("GOOGLE_REGION") 
+  zone    = env("GOOGLE_ZONE")
+  credentials = base64decode(env("GOOGLE_CREDENTIALS_BASE64"))
+}
