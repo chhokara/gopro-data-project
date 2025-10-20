680548b37be13a7e1cb5d4bc8897e1f8cd37809e
chhokara <chhokara@gmail.com>
2025-10-19 18:48:34 -0700

    build out pubsub

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,4 +18,18 @@ module "raw_bucket" {
     layer       = "raw"
     project     = "gopro-data"
   }
+  notification = {
+    topic = "gopro-data-topic"
+    payload_format = "JSON_API_V1"
+    event_types = ["OBJECT_FINALIZE"]
+    object_name_prefix = "raw/"
+  }
+}
+
+module "pubsub" {
+  source                = "./modules/pubsub"
+  project_id            = "gopro-data-project"
+  topic_name            = "gopro-data-topic"
+  subscription_name     = "gopro-data-subscription"
+  # subscriber_service_account = "service-account-email"
 }
---
+++ terraform/modules/pubsub/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "The ID of the project in which the resource belongs."
+  type        = string
+}
+
+variable "topic_name" {
+  description = "The name of the Pub/Sub topic."
+  type        = string
+}
+
+variable "subscription_name" {
+  description = "The name of the Pub/Sub subscription."
+  type        = string
+}   
+
+# variable "subscriber_service_account" {
+#   description = "The service account email to be granted subscriber role."
+#   type        = string
+# }   
