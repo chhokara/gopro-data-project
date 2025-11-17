c9aca8ed3054e55017fea8cfe43b6e622b06a46d
chhokara <chhokara@gmail.com>
2025-11-16 16:59:34 -0800

    create on demand dag

--- composer_environment/gopro-gpmf-extract-on-demand.py
+++ composer_environment/gopro-gpmf-extract-on-demand.py
@@ -36,7 +36,6 @@ AR_TAG = Variable.get(
     "AR_TAG", default_var="v1")
 CONTAINER_IMAGE = f"{REGION}-docker.pkg.dev/{APP_PROJECT_ID}/{AR_REPO}/{AR_IMAGE}:{AR_TAG}"
 
-
 with DAG(
     dag_id="gopro_gpmf_extract_on_demand_v1",
     start_date=datetime(2025, 1, 1),
@@ -45,4 +44,40 @@ with DAG(
     default_args=DEFAULT_ARGS,
     tags=["gopro", "gpmf", "extraction", "gcs", "on_demand"],
 ) as dag:
-    pass
+    def prepare_on_demand_run(ti, dag_run=None):
+        conf = dag_run.conf if dag_run else {}
+
+        bucket = conf.get("bucket", RAW_BUCKET)
+        name = conf.get("name")
+        out_prefix = conf.get("out_prefix", OUT_PREFIX)
+
+        if not name:
+            raise ValueError("Parameter 'name' is required.")
+
+        ti.xcom_push(key="bucket", value=bucket)
+        ti.xcom_push(key="name", value=name)
+        ti.xcom_push(key="out_prefix", value=out_prefix)
+
+    prepare_task = PythonOperator(
+        task_id="prepare_on_demand_run",
+        python_callable=prepare_on_demand_run,
+    )
+
+    run_gpmf_extraction = KubernetesPodOperator(
+        task_id="run_gpmf_extraction_on_demand",
+        name="gpmf-extract-on-demand",
+        namespace="composer-user-workloads",
+        config_file="/home/airflow/composer_kube_config",
+        image=CONTAINER_IMAGE,
+        image_pull_policy="IfNotPresent",
+        is_delete_operator_pod=True,
+        env_vars={
+            "RAW_BUCKET": "{{ ti.xcom_pull('prepare_on_demand_run', key='bucket')}}",
+            "CURATED_BUCKET": CURATED_BUCKET,
+            "OBJECT_NAME": "{{ ti.xcom_pull('prepare_on_demand_run', key='name')}}",
+            "OUT_PREFIX": "{{ ti.xcom_pull('prepare_on_demand_run', key='out_prefix')}}",
+        },
+        get_logs=True,
+    )
+
+    prepare_on_demand_run >> run_gpmf_extraction
