0512a908b391fdcd7f454c8eb3e48808d2ac8e0b
chhokara <chhokara@gmail.com>
2025-11-18 16:33:36 -0800

    deprovision

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,106 +1,106 @@
-locals {
-  required_services = [
-    "compute.googleapis.com",
-    "storage.googleapis.com",
-    "pubsub.googleapis.com",
-    "bigquery.googleapis.com",
-    "composer.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "containeranalysis.googleapis.com",
-  ]
-  project_id = "gopro-data-project"
-  region     = "us-central1"
-}
+# locals {
+#   required_services = [
+#     "compute.googleapis.com",
+#     "storage.googleapis.com",
+#     "pubsub.googleapis.com",
+#     "bigquery.googleapis.com",
+#     "composer.googleapis.com",
+#     "artifactregistry.googleapis.com",
+#     "containeranalysis.googleapis.com",
+#   ]
+#   project_id = "gopro-data-project"
+#   region     = "us-central1"
+# }
 
-resource "google_project_service" "required_services" {
-  for_each = toset(local.required_services)
-  project  = local.project_id
-  service  = each.key
+# resource "google_project_service" "required_services" {
+#   for_each = toset(local.required_services)
+#   project  = local.project_id
+#   service  = each.key
 
-  disable_dependent_services = true
-}
+#   disable_dependent_services = true
+# }
 
-module "raw_bucket" {
-  source            = "./modules/gcs-bucket"
-  name              = "gopro-raw-data-bucket"
-  project_id        = "gopro-data-project"
-  autoclass_enabled = true
-  lifecycle_rules = [
-    {
-      action = {
-        type = "Delete"
-      }
-      condition = {
-        age = 30
-      }
-    }
-  ]
-  labels = {
-    environment = "dev"
-    layer       = "raw"
-    project     = "gopro-data"
-  }
-  notification = {
-    topic          = module.pubsub.pubsub_topic
-    payload_format = "JSON_API_V1"
-    event_types    = ["OBJECT_FINALIZE"]
-  }
+# module "raw_bucket" {
+#   source            = "./modules/gcs-bucket"
+#   name              = "gopro-raw-data-bucket"
+#   project_id        = "gopro-data-project"
+#   autoclass_enabled = true
+#   lifecycle_rules = [
+#     {
+#       action = {
+#         type = "Delete"
+#       }
+#       condition = {
+#         age = 30
+#       }
+#     }
+#   ]
+#   labels = {
+#     environment = "dev"
+#     layer       = "raw"
+#     project     = "gopro-data"
+#   }
+#   notification = {
+#     topic          = module.pubsub.pubsub_topic
+#     payload_format = "JSON_API_V1"
+#     event_types    = ["OBJECT_FINALIZE"]
+#   }
 
-  force_destroy = true
+#   force_destroy = true
 
-  depends_on = [module.pubsub, google_project_service.required_services]
-}
+#   depends_on = [module.pubsub, google_project_service.required_services]
+# }
 
-module "curated_bucket" {
-  source            = "./modules/gcs-bucket"
-  name              = "gopro-curated-data-bucket"
-  project_id        = local.project_id
-  autoclass_enabled = true
-  lifecycle_rules = [
-    {
-      action = {
-        type = "Delete"
-      }
-      condition = {
-        age = 90
-      }
-    }
-  ]
-  labels = {
-    environment = "dev"
-    layer       = "curated"
-    project     = "gopro-data"
-  }
+# module "curated_bucket" {
+#   source            = "./modules/gcs-bucket"
+#   name              = "gopro-curated-data-bucket"
+#   project_id        = local.project_id
+#   autoclass_enabled = true
+#   lifecycle_rules = [
+#     {
+#       action = {
+#         type = "Delete"
+#       }
+#       condition = {
+#         age = 90
+#       }
+#     }
+#   ]
+#   labels = {
+#     environment = "dev"
+#     layer       = "curated"
+#     project     = "gopro-data"
+#   }
 
-  force_destroy = true
+#   force_destroy = true
 
-  depends_on = [google_project_service.required_services]
-}
+#   depends_on = [google_project_service.required_services]
+# }
 
-module "dags_bucket" {
-  source            = "./modules/gcs-bucket"
-  name              = "gopro-composer-dags-bucket"
-  project_id        = local.project_id
-  autoclass_enabled = false
-  labels = {
-    environment = "dev"
-    project     = "gopro-data"
-    purpose     = "composer-dags"
-  }
+# module "dags_bucket" {
+#   source            = "./modules/gcs-bucket"
+#   name              = "gopro-composer-dags-bucket"
+#   project_id        = local.project_id
+#   autoclass_enabled = false
+#   labels = {
+#     environment = "dev"
+#     project     = "gopro-data"
+#     purpose     = "composer-dags"
+#   }
 
-  force_destroy = true
+#   force_destroy = true
 
-  depends_on = [google_project_service.required_services]
-}
+#   depends_on = [google_project_service.required_services]
+# }
 
-module "pubsub" {
-  source            = "./modules/pubsub"
-  project_id        = local.project_id
-  topic_name        = "gopro-data-topic"
-  subscription_name = "gopro-data-subscription"
+# module "pubsub" {
+#   source            = "./modules/pubsub"
+#   project_id        = local.project_id
+#   topic_name        = "gopro-data-topic"
+#   subscription_name = "gopro-data-subscription"
 
-  depends_on = [google_project_service.required_services]
-}
+#   depends_on = [google_project_service.required_services]
+# }
 
 # module "composer" {
 #   source           = "./modules/composer"
@@ -130,21 +130,21 @@ module "pubsub" {
 #   depends_on = [module.raw_bucket, module.curated_bucket, module.dags_bucket, module.pubsub, google_project_service.required_services]
 # }
 
-resource "google_pubsub_subscription_iam_member" "this" {
-  project      = local.project_id
-  subscription = module.pubsub.pubsub_subscription
-  role         = "roles/pubsub.subscriber"
-  member       = "serviceAccount:${module.composer.composer_sa_email}"
+# resource "google_pubsub_subscription_iam_member" "this" {
+#   project      = local.project_id
+#   subscription = module.pubsub.pubsub_subscription
+#   role         = "roles/pubsub.subscriber"
+#   member       = "serviceAccount:${module.composer.composer_sa_email}"
 
-  depends_on = [module.pubsub, module.composer, google_project_service.required_services]
-}
+#   depends_on = [module.pubsub, module.composer, google_project_service.required_services]
+# }
 
-module "artifact_registry" {
-  source            = "./modules/artifact-registry"
-  project_id        = local.project_id
-  repository_id     = "gopro-artifact-repo"
-  region            = local.region
-  composer_sa_email = module.composer.composer_sa_email
+# module "artifact_registry" {
+#   source            = "./modules/artifact-registry"
+#   project_id        = local.project_id
+#   repository_id     = "gopro-artifact-repo"
+#   region            = local.region
+#   composer_sa_email = module.composer.composer_sa_email
 
-  depends_on = [module.composer, google_project_service.required_services]
-}
+#   depends_on = [module.composer, google_project_service.required_services]
+# }
