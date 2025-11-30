f36e63d042d2d149419080e8d01740c15fed4f81
chhokara <chhokara@gmail.com>
2025-11-29 18:55:49 -0800

    fix typos

--- terraform/modules/cloud-run-job/output.tf
+++ terraform/modules/cloud-run-job/output.tf
@@ -1,9 +1,9 @@
 output "gpmf_job_name" {
   description = "The name of the Cloud Run Job."
-  value       = google_cloud_run_job.gpmf_job.name
+  value       = google_cloud_run_v2_job.gpmf_job.name
 }
 
 output "gpmf_job_location" {
   description = "The location of the Cloud Run Job."
-  value       = google_cloud_run_job.gpmf_job.location
+  value       = google_cloud_run_v2_job.gpmf_job.location
 }
