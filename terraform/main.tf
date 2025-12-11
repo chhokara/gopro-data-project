da4108aab4b39bc5e09aca431f323066db62cce3
chhokara <chhokara@gmail.com>
2025-12-10 22:38:15 -0800

    add project level role

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,6 +126,12 @@ resource "google_project_iam_member" "airflow_pubsub_pub" {
   member  = "serviceAccount:${google_service_account.airflow_orchestrator.email}"
 }
 
+resource "google_project_iam_member" "gpmf_job_runner" {
+  project = local.project_id
+  role    = "roles/run.jobRunner"
+  member  = "serviceAccount:${google_service_account.airflow_orchestrator.email}"
+}
+
 module "cloud_run_job_service" {
   source             = "./modules/cloud-run-job"
   project_id         = local.project_id
