ac45282099628a1f5e392b956fef9b4b9d434e6e
chhokara <chhokara@gmail.com>
2025-12-13 19:23:58 -0800

    submit on demand DAG

---
+++ airflow/dags/gopro-gpmf-extract-manual.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+
+PROJECT_ID = "gopro-data-project"
+REGION = "us-central1"
+CLOUD_RUN_JOB_NAME = "gopro-data-processor-job"
+
+RAW_BUCKET = "gopro-raw-data-bucket"
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+
+def prepare_inputs(**context):
+    dag_run = context.get("dag_run")
+    conf = dag_run.conf if dag_run else {}
+
+    bucket = conf.get("bucket") or RAW_BUCKET
+    object_name = conf.get("object_name")
+
+    if not object_name:
+        raise ValueError("Provide object_name in DAG run config to point at an existing file.")
+
+    ti = context["ti"]
+    ti.xcom_push(key="bucket", value=bucket)
+    ti.xcom_push(key="object_name", value=object_name)
+
+
+with DAG(
+    dag_id="gopro_gpmf_extract_manual",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    default_args=default_args,
+    description="On-demand trigger to process an existing GoPro video already in raw GCS.",
+) as dag:
+    parse_conf = PythonOperator(
+        task_id="prepare_inputs",
+        python_callable=prepare_inputs,
+        provide_context=True,
+    )
+
+    run_gpmf_extraction_job = CloudRunExecuteJobOperator(
+        task_id="run_gpmf_extraction_job",
+        project_id=PROJECT_ID,
+        region=REGION,
+        job_name=CLOUD_RUN_JOB_NAME,
+        gcp_conn_id="google_cloud_default",
+        overrides={
+            "container_overrides": [
+                {
+                    "env": [
+                        {
+                            "name": "RAW_BUCKET",
+                            "value": "{{ ti.xcom_pull(task_ids='prepare_inputs', key='bucket') }}",
+                        },
+                        {
+                            "name": "OBJECT_NAME",
+                            "value": "{{ ti.xcom_pull(task_ids='prepare_inputs', key='object_name') }}",
+                        },
+                    ]
+                }
+            ]
+        },
+    )
+
+    parse_conf >> run_gpmf_extraction_job
