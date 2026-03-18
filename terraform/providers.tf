f8dba86b64584418a763012662900ffd456e1623
chhokara <chhokara@gmail.com>
2026-03-18 14:08:13 -0700

    comment out archive provider for now

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.0"
-    }
+    # archive = {
+    #   source  = "hashicorp/archive"
+    #   version = "~> 2.0"
+    # }
   }
 
   cloud {
